1e365396c9b4089f4442c660d6aee54e478154ca
RSkendzic <rskendzic@execom.eu>
2016-08-08 12:10:26 +0200

    Component for list items

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,24 +1,26 @@
 import { Component } from '@angular/core';
+import { CustomerComponent } from './customer/customer.component' ;
 
 @Component({
     selector: 'my-app',
-    templateUrl: "app/app.component.html"
+    templateUrl: "app/app.component.html",
+    directives: [CustomerComponent]
 })
 export class AppComponent {
 
 title: string="Customer app";
 name: string ="Radovan";
-radovanColor = "yellow";
+radovanColor = "green";
 customers = [
   {id:1, name:"Radovan"},
   {id:2, name:"Jelena"},
   {id:3, name:"Tara"},
-  {id:4, name:"Vladimir"}
+  {id:4, name:"Vladimir"},
   {id:5, name:"Nenad"}
 ];
 
 changeSuitColor()
   {
-    this.radovanColor = this.radovanColor === "red" ? "yellow":"red";
+    this.radovanColor = this.radovanColor === "red" ? "green":"red";
   }
 }
---
+++ app/customer/customer.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: 'app/customer/customer.component.html',
+})
+export class CustomerComponent implements OnInit {
+  @Input() customer: {id: number, name: string};
+  constructor() {  }
+
+  ngOnInit() {}
+}
